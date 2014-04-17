2f0f386c307929b9b5aaa13e718c0429bd07d2a4
Neo Chan <netkiller@msn.com>
2014-04-17 22:40:09 +0800

    batch command

---
+++ test/mototrbo.bat
@@ -0,0 +1,8 @@
+route delete 12.0.0.0
+route add 12.0.0.0 mask 255.0.0.0 192.168.11.1
+rem route delete 192.168.10.0
+rem route add 192.168.10.0 mask 255.255.255.0 192.168.11.1
+rem route add 12.0.0.0 mask 255.0.0.0 192.168.10.1
+
+rem -p
+pause
