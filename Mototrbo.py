edf6ae2c80ffc3b37689fe75b752727896c9eb7b
Neo Chan <netkiller@msn.com>
2016-12-01 22:57:34 +0800

    Update Mototrbo.py

--- Mototrbo.py
+++ Mototrbo.py
@@ -9,6 +9,7 @@ try:
 	from struct import *
 	from optparse import OptionParser, OptionGroup
 	from Protocol import *
+	import os
 except ImportError as err:
 	print("Error: %s" %(err))    
 
@@ -42,9 +43,15 @@ class Mototrbo() :
 		group = OptionGroup(self.parser, 'LRRP', ' Location Request and Response Protocol (LRRP)')
 		#group.add_option('-c','--cai', dest="cai", metavar="12", help='CAI Network', default=12)
 		self.parser.add_option_group(group)
+
+		group = OptionGroup(self.parser, 'Network', 'Network and route table ext.')
+		group.add_option('-r','--route', dest='route', action='store_true', help='route print')
+		group.add_option('-p','--ping', dest="ping", metavar="", help='ping ip address', default="")
+		group.add_option('-o','--online', dest="online", metavar="", help="Check radio's status.", default="")
+		self.parser.add_option_group(group)
 		
-		#self.parser.add_option('-v','--version',action='store_true', help='print version number')
-		#self.parser.add_option('-d','--daemon', dest='daemon', action='store_true', help='run as daemon')
+		
+		
 		#self.parser.add_option('','--logfile', help='logs file.', default='backup.log')
 		pass
 	def tms(self):
@@ -58,27 +65,37 @@ class Mototrbo() :
 			"\n  Example: ",
 			"\t-c 12 -t 7558688 Hello",
 			"\t--id2ip=1001",
-			"\t--ip2id=12.115.86.232"
+			"\t--ip2id=12.115.86.232",
+                        "\t-c 12 -r",
+                        "\t-p 12.115.83.17",
+                        "\t-c 12 -o 7558008"
 		]
 		for line in lines:
 			print(line)
 		print("\n  Homepage: http://netkiller.github.io\tAuthor: Neo <netkiller@msn.com>")		
 	def main(self):
 		(self.options, args) = self.parser.parse_args()
-		print("===================================")
-		print(self.options, args)
+		#print("===================================")
+		#print(self.options, args)
 		#self.usage()
 		#print(self.config)
-		print("===================================")
+		#print("===================================")
+		protocol = Protocol()
+		
 		if self.options.id2ip :
-			protocol = Protocol()
-			print(protocol.id2ip(int(self.options.id2ip)))
-		if self.options.ip2id :
-			protocol = Protocol()
+			print(protocol.id2ip(int(self.options.cai), int(self.options.id2ip)))
+		elif self.options.ip2id :
 			print(protocol.ip2id(str(self.options.ip2id)))
-		if self.options.tms :
+		elif self.options.tms :
 			tms = TMS()
 			tms.sendtoid(int(self.options.cai), int(self.options.tms), args[0])
+		elif self.options.route :
+                        print("Please add the following route:")
+                        print("route add %s.0.0.0 mask 255.0.0.0 192.168.11.1" % self.options.cai)
+		elif self.options.ping :
+                        os.system("ping %s" % self.options.ping)
+		elif self.options.online :
+                        os.system("ping %s" % protocol.id2ip(int(self.options.cai), int(self.options.online))) 
 		else:
 			self.usage()
 		
@@ -87,6 +104,5 @@ if __name__ == '__main__':
 		mototrbo = Mototrbo()
 		mototrbo.main()
 		
-
 	except KeyboardInterrupt:
 		print ("Crtl+C Pressed. Shutting down.")
